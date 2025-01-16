f52d9ad6be04dbf7fe1c427c095e06672173afe2
Faizan Mustafa <faizanmustafa324@gmail.com>
2025-01-16 19:56:22 +0500

    Created Supplication type

---
+++ src/types/supplication.type.ts
@@ -0,0 +1,10 @@
+export type Supplication = {
+  id: number;
+  content_ar: string;
+  pron_en: string;
+  trans_en: string;
+  trans_ur: string;
+  verse_no: string;
+  surah_name_ar: string;
+  surah_name_en: string;
+};
