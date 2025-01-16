08eacf8a35d8b47d09be33e0bb2e18ed78cde134
Faizan Mustafa <faizanmustafa324@gmail.com>
2025-01-16 19:54:37 +0500

    Implemented theme based styling

---
+++ src/theme.ts
@@ -0,0 +1,26 @@
+import {Theme} from './types/theme.type';
+
+export const theme: Theme = {
+  background: {
+    colors: {
+      offWhite: '#f8f8f8',
+      white: '#fff',
+    },
+  },
+  border: {
+    colors: {
+      ashWhite: '#f1f1f1',
+    },
+  },
+  typography: {
+    font: {
+      families: {
+        urdu: 'AlQalamNastaleeq',
+        arabic: 'AlQalamQuran',
+      },
+      colors: {
+        grey: '#808080',
+      },
+    },
+  },
+};
